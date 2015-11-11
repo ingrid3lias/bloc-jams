160658fd27c4ee64c68595510b15625a6c2173ff
Ingrid Elias <elias.ingrid@yahoo.com>
2015-11-11 16:10:50 +0900

    Assignment 23 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(item){
+                item.style.opacity = 1;
+                item.style.transform = "scaleX(1) translateY(0)";
+                item.style.msTransform = "scaleX(1) translateY(0)";
+                item.style.WebkitTransform = "scaleX(1) translateY(0)";
+            };
+
 var animatePoints = function(points){
-                
-                var revealPoint = function(i){
-                    for(var i = 0; i < points.length; i++){
-                    points[i].style.opacity = 1;
-                    points[i].style.transform = "scaleX(1) translateY(0)";
-                    points[i].style.msTransform = "scaleX(1) translateY(0)";
-                    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-                };
-                }
-                revealPoint(); 
+                forEach(points, revealPoint);
             };
                 window.onload = function() {
                     
