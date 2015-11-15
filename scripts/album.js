30fae0c1a223b840cafbda93282b4d2db99918f3
Ingrid Elias <elias.ingrid@yahoo.com>
2015-11-15 16:36:45 +0900

    Assignment 25 Partial Complete

--- scripts/album.js
+++ scripts/album.js
@@ -18,19 +18,37 @@ var albumPicasso = {
 //Another Example Album
 
 var albumMarconi = {
-  name: 'The Telephone',
+    name: 'The Telephone',
     artist: 'Guglielmo Marconi',
     label: 'EM',
     year: '1909',
     albumArtUrl: 'assets/images/album_covers/20.png',
     songs: [
         { name: 'Hello, Operator?', length: '1:01' },
-        { name: 'Ring, ring, ring', lenght: '5:01' },
+        { name: 'Ring, ring, ring', length: '5:01' },
         { name: 'Fits in your pocket', length: '3:21' },
-        { name: 'Can you hear me now?', lenght: '3:14' },
+        { name: 'Can you hear me now?', length: '3:14' },
         { name: 'Wrong phone number', length: '2:15' }
     ]
 };
+
+//My Album Example
+
+var albumDeathCab = {
+  name: 'Kintsugi',
+    artist: 'Death Cab for Cutie',
+    label: 'Atlantic Records',
+    year: '2015',
+    albumArtUrl:'assets/images/album_covers/02.png',
+    songs: [
+        { name: 'No Room In Frame', length: '4:05' },
+        { name: 'Black Sun', length: '4:49' },
+        { name: 'The Ghosts Of Beverly Drive', length: '4:03' },
+        { name: 'Little Wanderer', length: '4:18' },
+        { name: 'You\'ve Haunted Me All My Life', length: '4:07' }
+    ]
+};
+
 
 var createSongRow = function(songNumber, songName, songLength) {
   var template = 
@@ -44,13 +62,13 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
-var setCurrentAlbum = function(album) {
-    //1
     var albumTitle = document.getElementsByClassName('album-view-title')[0];
     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
     var albumImage = document.getElementsByClassName('album-cover-art')[0];
     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+var setCurrentAlbum = function(album) {
     //2
     albumTitle.firstChild.nodeValue = album.name;
     albumArtist.firstChild.nodeValue = album.artist;
@@ -63,14 +81,22 @@ var setCurrentAlbum = function(album) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
     }
 };
-
+        
 window.onload = function(){
     setCurrentAlbum(albumPicasso);
+    
+var albumList = [albumPicasso, albumMarconi, albumDeathCab];
+
+    function nextAlbum(element){
+      setCurrentAlbum(element);
+   };
+    
+    albumImage.addEventListener('click', function(){ 
+        albumList.forEach(nextAlbum);
+        
+    });
+
 };
-
-
-
-
 
 
 
